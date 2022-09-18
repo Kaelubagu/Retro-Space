469cc3d6839fdffc6a7a181970da38d290e4bcb6
Kaelubagu <caguiar@trinity.edu>
2022-09-18 14:11:04 -0500

    Main Computer one

--- src/main/scala/cs2/units/Volume.scala
+++ src/main/scala/cs2/units/Volume.scala
@@ -5,42 +5,42 @@ class Volume() {
   private var lit:Double = 0.0
 
   //Basic math operators to add, subtract, and scale volumes
-  def + (other:Volume):Volume = { ??? }
-  def += (other:Volume):Unit  = { ??? }
+  def + (other:Volume):Volume = {this.list + other.lit}
+  def += (other:Volume):Unit  = {this.Lit = this.lit + other.Lit }
 
-  def - (other:Volume):Volume = { ??? }
-  def -= (other:Volume):Unit  = { ??? }
+  def - (other:Volume):Volume = {this.list - other.lit}
+  def -= (other:Volume):Unit  = {this.Lit = this.lit - other.Lit}
 
-  def * (scalar:Double):Volume = { ??? }
-  def *= (scalar:Double):Unit  = { ??? }
+  def * (scalar:Double):Volume = {this.list * other.lit}
+  def *= (scalar:Double):Unit  = {this.Lit = this.lit * other.Lit}
 
-  def / (scalar:Double):Volume = { ??? }
-  def /= (scalar:Double):Unit  = { ??? }
+  def / (scalar:Double):Volume = {this.list / other.lit}
+  def /= (scalar:Double):Unit  = {this.list = this.list / other.lit}
 
-  //Getter functions that return in a variety of units
-  def liters():Double = { ??? }
-  def milliliters():Double = { ??? }
-  def gallons():Double = { ??? }
-  def quarts():Double = { ??? }
-  def pints():Double = { ??? }
-  def cups():Double = { ??? }
-  def teaspoons():Double = { ??? }
-  def tablespoons():Double = { ??? }
+ //Getter functions that return in a variety of units
+  def liters():Double = { lit }
+  def milliliters():Double = { lit * 1000 }
+  def gallons():Double = { lit / 3.785 }
+  def quarts():Double = { lit * 1.057 }
+  def pints():Double = { lit * 2.113 }
+  def cups():Double = { lit * 4.227 }
+  def teaspoons():Double = { lit * 202.9  }
+  def tablespoons():Double = { lit * 67.628 }
 }
 
 object Volume {
   //"Constructor" apply methods operating in liters
-  def apply():Volume = { ??? }
-  def apply(amt:Double):Volume = { ??? }
+  def apply():Volume = { new Volume }
+  def apply(amt:Double):Volume = { new Volume() }
 
   //Alternative "static" methods to create volumes in other units
-  def liters(amt:Double):Volume = { ??? } //identical to an apply method
-  def milliliters(amt:Double):Volume = { ??? }
-  def gallons(amt:Double):Volume = { ??? }
-  def quarts(amt:Double):Volume = { ??? }
-  def pints(amt:Double):Volume = { ??? }
-  def cups(amt:Double):Volume = { ??? }
-  def teaspoons(amt:Double):Volume = { ??? }
-  def tablespoons(amt:Double):Volume = { ??? }
+  def liters(amt:Double):Volume = { new apply() } //identical to an apply method
+  def milliliters(amt:Double):Volume = { amt * 1000 }
+  def gallons(amt:Double):Volume = { lit / 3.785 }
+  def quarts(amt:Double):Volume = { lit * 1.057 }
+  def pints(amt:Double):Volume = { lit * 2.113 }
+  def cups(amt:Double):Volume = { lit * 4.227 }
+  def teaspoons(amt:Double):Volume = { lit * 202.9 }
+  def tablespoons(amt:Double):Volume = { lit * 67.628 }
 }
 
